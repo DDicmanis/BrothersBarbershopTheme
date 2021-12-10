9b266bc91ff4eab7ede7deb9cbfe21d4b2d592cf
ArrogantAmbassadorLV <daniels.dicmanis@gmail.com>
2021-12-10 17:34:25 +0200

    header+fixes

---
+++ assets/header-controll.js
@@ -0,0 +1,95 @@
+window.onscroll = function () { myFunction() };
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+var lastScrollTop = 0;
+var winX = null;
+var winY = null;
+var drawer = false;
+class DomUtils {
+  // left: 37, up: 38, right: 39, down: 40,
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  static keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+  static preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault) e.preventDefault();
+    e.returnValue = false;
+  }
+
+  static preventDefaultForScrollKeys(e) {
+    if (DomUtils.keys[e.keyCode]) {
+      DomUtils.preventDefault(e);
+      return false;
+    }
+  }
+
+  static disableScroll() {
+    document.addEventListener('wheel', DomUtils.preventDefault, {
+      passive: false,
+    }); // Disable scrolling in Chrome
+    document.addEventListener('keydown', DomUtils.preventDefaultForScrollKeys, {
+      passive: false,
+    });
+  }
+
+  static enableScroll() {
+    document.removeEventListener('wheel', DomUtils.preventDefault, {
+      passive: false,
+    }); // Enable scrolling in Chrome
+    document.removeEventListener(
+      'keydown',
+      DomUtils.preventDefaultForScrollKeys,
+      {
+        passive: false,
+      }
+    ); // Enable scrolling in Chrome
+  }
+}
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function disableScroll() {
+  document.body.addEventListener('touchmove', preventDefault, { passive: false });
+  document.addEventListener('wheel', DomUtils.preventDefault, { passive: false });
+}
+function enableScroll() {
+  document.body.removeEventListener('touchmove', preventDefault, { passive: false });
+  document.removeEventListener('wheel', DomUtils.preventDefault, { passive: false });
+}
+function myFunction() {
+
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  if (st > lastScrollTop && st > 100) {
+    header.style.display = "none"
+
+  } else {
+    header.style.display = "flex"
+
+  }
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+  lastScrollTop = st <= 0 ? 0 : st;
+}
+function openNav() {
+  drawer = true;
+  disableScroll();
+  if (window.innerWidth < 915) {
+    document.getElementById("drawer").style.width = "100%";
+    document.getElementById("drawer").style.padding = "25px 20px 0px 0px";
+  } else {
+    document.getElementById("drawer").style.width = "40%";
+    document.getElementById("drawer").style.padding = "20px 140px 0px 0px";
+  }
+
+}
+
+function closeNav() {
+  drawer = false;
+  enableScroll();
+  document.getElementById("drawer").style.width = "0";
+  document.getElementById("drawer").style.padding = "0";
+}
