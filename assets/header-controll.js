c5c7e96079c9a844834f90231f5f8475e5a466c6
ArrogantAmbassadorLV <daniels.dicmanis@gmail.com>
2021-12-22 15:34:31 +0200

    cart/singleblog

--- assets/header-controll.js
+++ assets/header-controll.js
@@ -57,20 +57,50 @@ function enableScroll() {
   document.body.removeEventListener('touchmove', preventDefault, { passive: false });
   document.removeEventListener('wheel', DomUtils.preventDefault, { passive: false });
 }
+var languge = document.querySelector(".header .language-heading")
+var navLi = document.querySelectorAll(".header .header-inline li a")
+myFunction()
 function myFunction() {
-
+  var burgerWhite = document.getElementById("whiteBurger")
+  var burgerBlack = document.getElementById("blackBurger")
   var st = window.pageYOffset || document.documentElement.scrollTop;
-  if (st > lastScrollTop && st > 100) {
-    header.style.display = "none"
-
+  if (st > lastScrollTop && st > 500) {
+    header.classList.remove("transition")
+    // header.style.display = "none"
+    header.style.visibility = "hidden"
+    header.style.opacity = "0"
+    burgerWhite.style.display = "none"
+    languge.style.color = "black"
+    burgerBlack.style.display = "block"
+    navLi[0].style.color = "black"
+    navLi[1].style.color = "black"
   } else {
-    header.style.display = "flex"
+    header.classList.add("transition")
+    // header.style.display = "flex"
+    header.style.visibility = "visible"
+    header.style.opacity = "1"
 
   }
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+
   } else {
     header.classList.remove("sticky");
+    if (window.location.pathname === '/') {
+      burgerWhite.style.display = "block"
+      burgerBlack.style.display = "none"
+      languge.style.color = "white"
+      navLi[0].style.color = "white"
+      navLi[1].style.color = "white"
+
+    } else {
+      burgerWhite.style.display = "none"
+      burgerBlack.style.display = "block"
+      languge.style.color = "black"
+      navLi[0].style.color = "black"
+      navLi[1].style.color = "black"
+    }
+
   }
   lastScrollTop = st <= 0 ? 0 : st;
 }
